8e2c0da05b85b20cf8563b944441eee2d4106a98
WorkSpacePython <78704527+oglinked@users.noreply.github.com>
2024-04-30 18:55:21 +0300

    New version

--- CodeCamp/luhn_algorithm.py
+++ CodeCamp/luhn_algorithm.py
@@ -10,8 +10,15 @@ Canadian Social Insurance Numbers...
 """
 
 # https://www.freecodecamp.org/learn/scientific-computing-with-python/
+
 # https://en.wikipedia.org/wiki/Luhn_algorithm
 # https://www.geeksforgeeks.org/luhn-algorithm/
+
+# https://www.w3schools.com/python/ref_string_translate.asp
+# https://www.tutorialsteacher.com/python/string-maketrans
+# https://www.w3schools.com/python/ref_string_maketrans.asp
+# https://www.geeksforgeeks.org/python-string-maketrans-method/
+# https://www.programiz.com/python-programming/methods/string/maketrans
 
 # The Luhn algorithm is as follows:
 
@@ -31,7 +38,7 @@ Canadian Social Insurance Numbers...
 import os
 
 
-def verify_card_number(card_number):
+def verify_card_number(card_number) -> bool:
     """The Luhn algorithm.
     A Luhn algorithm is used to verify credit card numbers or other
     identification numbers, such as Aadhaar number.
@@ -57,20 +64,39 @@ def verify_card_number(card_number):
     return total % 10 == 0
 
 
+def pre_cleaning(string_to_check) -> str:
+    """This Function checks the Input (string_to_check) and,
+    if there are not eligible symbols in it, removes those
+    (replace on '').
+    """
+    string_to_check = str(string_to_check)  # Convert to <class 'str'>.
+
+    # Replace "-", " " and "\t" on "" in the Input:
+    value_translation = str.maketrans({'-': '', ' ': '', '\t': ''})
+    result_string = string_to_check.translate(value_translation)
+
+    if len(result_string) < len(string_to_check):
+        print('WARNING: Some not eligible symbols were removed \
+from the Input!')
+    else:
+        pass
+
+    return result_string
+
+
 def int_validation(value) -> int:
-    """This Function tests if an inputted value may belong 
+    """This Function tests if an Input (value) may belong 
     to <class 'int'> and be a zero or positive integer. 
     If not, then it asks to repeat Input.
     """
-    # Pre-cleaning. Removing "-" and " " from the Input:
-    value_translation = str.maketrans({'-': '', ' ': ''})
-    value = value.translate(value_translation)
+    # # Pre-cleaning. Removing "-", "\t" and " " from the Input:
+    value = pre_cleaning(value)
 
     try:
         value = int(value)
     except:
-        value = input('ERROR: Only integer numbers are allowed. \
-\nRepeat Input: ')
+        value = input('ERROR: Only decimal digits \
+[0,1,2,3,4,5,6,7,8,9] are allowed. \nRepeat Input: ')
         value = int_validation(value)  # Recursion!
 #     else:
 #         if value < 0:
@@ -88,7 +114,7 @@ def luhn_algorithm():
 
     os.system('cls')  # Clearing the Screen.
     # The Greeting.
-    print('\nHello Host!\nYou run version 01.02 of \
+    print('\nHello Host!\nYou run the version 01.03 of \
 the program luhn_algorithm.py.')
 
     # The main loop:
@@ -98,14 +124,15 @@ the program luhn_algorithm.py.')
         card_number = input('\nInput Card Number: ')
 
         # The Pre-Validation:
-        # Only positive integers are allowed in the Card Number.
+        # Only decimal digits [0,1,2,3,4,5,6,7,8,9] are allowed in
+        # the Card Number, but card_number should be <class str>.
         card_number = str(int_validation(card_number))
 
         # The Call & Output block (if True, then VALID!):
         if verify_card_number(card_number):
-            print('VALID!')
+            print(f'\nCard Number {card_number} is VALID!')
         else:
-            print('INVALID!')
+            print(f'\nCard Number {card_number} is INVALID!')
 
         # The end of the main loop.
         k = input('\nWould you like to input another \
